34794eb5156929d56d52742873ee8ff79d62795c
ganlin <1316245375@qq.com>
2021-02-04 17:20:26 +0800

    flink_1

--- src/main/java/com/test/util/IntegerFunc.java
+++ src/main/java/com/test/util/IntegerFunc.java
@@ -1,7 +1,7 @@
 package com.test.util;
 
 public interface IntegerFunc {
-    Integer comput(Integer num1,Integer nums2);
+    Integer comput(Integer num1,Integer num2);
     default String getInfo(){
         return "两数处理函数" ;
     }
--- src/main/java/com/test/util/SegmentTreeInt.java
+++ src/main/java/com/test/util/SegmentTreeInt.java
@@ -5,15 +5,16 @@ import java.util.Arrays;
 public class SegmentTreeInt {
     int n, t[];
     IntegerFunc sfunc;
+    int def_val;
     //The procedure for constructing the Segment Tree from a given array a[] looks like this:
     // it is a recursive function with the parameters a[] (the input array), v (the index of the current vertex),
     // and the boundaries tl and tr of the current segment. In the main program this function will be called with
     // the parameters of the root vertex: v=1, tl=0, and tr=n−1.
-    public SegmentTreeInt(int a[], int v, int tl, int tr,IntegerFunc func,int def_val) {
+    public SegmentTreeInt(int a[], int v, int tl, int tr,IntegerFunc func,int in_def_val) {
+        def_val=in_def_val;
         sfunc=func;
         n=a.length;
         t=new int[4*n];
-        Arrays.fill(t, def_val);
         build(a,v,tl,tr);
     }
     void build(int a[], int v, int tl, int tr) {
@@ -35,7 +36,7 @@ public class SegmentTreeInt {
     // using an additional check at the beginning of the function.
     int query(int v, int tl, int tr, int l, int r) {
         if (l > r)
-            return 0;
+            return def_val;
         if (l == tl && r == tr) {
             return t[v];
         }
